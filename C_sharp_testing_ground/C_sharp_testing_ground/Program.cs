c316d37e4041851246e54807582e5d35f06d5ed1
Rafael Hadamik <cedresque@gmail.com>
2021-01-28 14:35:51 +0100

    first commit, Hello world!

---
+++ C_sharp_testing_ground/C_sharp_testing_ground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_testing_ground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Hello clean World!");
+            Console.ReadKey();
+        }
+    }
+}
